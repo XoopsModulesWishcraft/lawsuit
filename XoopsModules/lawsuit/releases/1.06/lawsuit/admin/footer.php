b1d16c61ea5842bf7135610284c49cfb86a96826
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-07-03 10:06:59 +0000

    XOOPS 2.3.4 Refresh - Minor Fixes and Changes

---
+++ XoopsModules/lawsuit/releases/1.06/lawsuit/admin/footer.php
@@ -0,0 +1,9 @@
+<?php
+// $Id: footer.php,v 1.05 2009/06/24 23:45:00 wishcraft Exp $
+
+$credits = "<div style='text-align: right; font-size: x-small; margin-top: 15px;'>Powered by <a href='about.php'>Lawsuit ".$version."</a>";
+echo $credits;
+
+echo '</div>';
+footer_adminMenu();
+?>
---
+++ XoopsModules/lawsuit/releases/1.06/lawsuit/include/forms.php
@@ -0,0 +1,14 @@
+<?php
+
+	include_once XOOPS_ROOT_PATH."/class/xoopsformloader.php";
+	
+	// Include Internal Form Objects
+	include_once XOOPS_ROOT_PATH."/modules/lawsuit/class/formselectcategory.php";
+	include_once XOOPS_ROOT_PATH."/modules/lawsuit/class/formselectforms.php";
+	include_once XOOPS_ROOT_PATH."/modules/multisite/class/formselectdomains.php";
+
+	// Include Forms
+	include_once XOOPS_ROOT_PATH."/modules/lawsuit/include/categories.forms.php"	;
+	include_once XOOPS_ROOT_PATH."/modules/lawsuit/include/pages.forms.php"	;	
+			
+?>
